add704ceb02089b77404010c737b6ddb7b963e1a
ulrichru <48566240+ulrichru@users.noreply.github.com>
2024-10-04 23:25:56 +0000

    Challenge 1 gelöst

--- 01_01/01_01.py
+++ 01_01/01_01.py
@@ -1,4 +1,12 @@
-import sys
+while True:
+  try:
+    wert = int(input("Bitte geben Sie eine Zahl zwischen 0 und 9 ein: "))
+    if wert > 0 and wert <= 9:
+      print ("passt!!")
+      break
+    else:
+      print("Das ist keine Zahl zwischen 0 und 9!!")
+  except ValueError:
+    print("Zu dumm eine Zahl einzugeben??")
 
-print("Bitte geben Sie eine Zahl zwischen 0 und 9 ein: ")
-wert = input()
+  
